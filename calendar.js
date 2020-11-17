5bd5cd066aed186bb14da92cd25f629a5d9d5dcf
Moa Stenqvist <moa@stonetwig.se>
2020-11-17 16:46:23 +0100

    Add basic set up, html skeleton and files needed.

---
+++ calendar.js
@@ -0,0 +1,1 @@
+// Here goes functions etc regarding calendar / main.
---
+++ main.js
@@ -0,0 +1,7 @@
+// Here goes all initial functions and global variables.
+
+window.addEventListener('load', main);
+
+function main() {
+
+};
