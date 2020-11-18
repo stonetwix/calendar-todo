fa0af4b0b6090a31b01f8d1657232a67c52b2ce9
Moa Stenqvist <moa@stonetwig.se>
2020-11-18 14:54:30 +0100

    Add function, data state to main.js

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
-// Here goes all initial functions and global variables.
+// Here goes all initial functions, states and global variables.
 
 window.addEventListener('load', main);
 
 function main() {
 
 };
+
+
+let calendarState = {
+    '2020-11-17': [{title: 'Work'}],
+}
+
+let selectedDay = '';
+
+function addToDoToday(day, todo) {
+    if (calendarState[day] === undefined) {
+        calendarState[day] = [todo];
+    } else {
+        calendarState[day].push(todo);
+    }
+}
