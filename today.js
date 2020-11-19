2b044779bb238373dd2e993b48ebe49070900671
Nisa Tokmak <katnissa@gmail.com>
2020-11-19 16:04:17 +0100

    welcome section basic codes are added

--- today.js
+++ today.js
@@ -1,1 +1,25 @@
 // Here goes JS for welcome/today section
+var today = new Date();
+
+var months = new Array(12);
+months[0] = "Januari";
+months[1] = "Februari";
+months[2] = "Mars";
+months[3] = "April";
+months[4] = "Maj";
+months[5] = "Juni";
+months[6] = "Juli";
+months[7] = "Augusti";
+months[8] = "September";
+months[9] = "October";
+months[10] = "November";
+months[12] = "December";
+
+var days = new Array(7);
+days[0] = "Söndag";
+days[1] = "Måndag";
+days[2] = "Tisdag";
+days[3] = "Onsdag";
+days[4] = "Torsdag";
+days[5] = "Fredag";
+days[6] = "Lördag";
